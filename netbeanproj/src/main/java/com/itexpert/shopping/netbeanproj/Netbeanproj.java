a747ec0cd0d0a653afafc5b85a0bfe4f35247d29
leelapraba28 <leelads88@gmail.com>
2025-02-25 21:54:00 -0500

    initial project

---
+++ netbeanproj/src/main/java/com/itexpert/shopping/netbeanproj/Netbeanproj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexpert.shopping.netbeanproj;
+
+/**
+ *
+ * @author leela
+ */
+public class Netbeanproj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
